f51edd06a95deb202757c63d7c166b57263854cb
Chanchal Biswas <46987926+cncldeveloper@users.noreply.github.com>
2022-08-05 18:15:35 +0600

    Delete nubtk directory

--- nubtk/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database Connection
-try {
-    $db = new PDO("mysql:host=162.241.194.27;dbname=cybersec_routine", "cybersec_routine", "Na3Vh?SX[2@H");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    exit("Database Connection failed: " . $e->getMessage());
-}
