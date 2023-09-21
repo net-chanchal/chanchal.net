56ce3ad03c9f92be0330aa064b35f0aaebbc0c29
Md. Chanchal Biswas <mchanchalbd@gmail.com>
2023-09-21 16:40:11 +0600

    NUBTK Routine App

---
+++ nubtk/routine-app/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./style.css",
+                "./script.js",
+                "./images/filter.svg",
+                "./images/gear.svg",
+                "./images/home.svg",
+                "./images/icon-192x192.png",
+                "./images/icon-256x256.png",
+                "./images/icon-384x384.png",
+                "./images/icon-512x512.png"
+            ]);
+        })
+    )
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+})
