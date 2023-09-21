f9e7ac4ea2891c378ade4eda5f56e04249d7020a
Md. Chanchal Biswas <mchanchalbd@gmail.com>
2023-09-21 17:07:52 +0600

    NUBTK Routine App

--- nubtk/routine-app/script.js
+++ nubtk/routine-app/script.js
@@ -1,7 +1,7 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        // console.log(registration);
-    }).catch(error => {
-        console.log(error);
-    });
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register("sw.js").then(registration => {
+//         // console.log(registration);
+//     }).catch(error => {
+//         console.log(error);
+//     });
+// }
